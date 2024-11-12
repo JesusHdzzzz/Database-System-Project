756c9cd33df245069102a128fdceece4f9e69d0b
Jesus Hernandez <114837227+JesusHdzzzz@users.noreply.github.com>
2024-11-11 20:23:03 -0800

    Query to delete all data from db

---
+++ delete-data.sql
@@ -0,0 +1,29 @@
+DELETE FROM CreditDebitCards;
+delete from sqlite_sequence where name='CreditDebitCards';
+
+DELETE FROM DevicesLocations;
+delete from sqlite_sequence where name='DevicesLocations';
+
+DELETE FROM Group_Passwords;
+delete from sqlite_sequence where name='Group_Passwords';
+
+DELETE FROM Group_Users;
+delete from sqlite_sequence where name='Group_Users';
+
+DELETE FROM "Groups";
+delete from sqlite_sequence where name='Groups';
+
+DELETE FROM History;
+delete from sqlite_sequence where name='History';
+
+DELETE FROM Passwords;
+delete from sqlite_sequence where name='Passwords';
+
+DELETE FROM User_Websites;
+delete from sqlite_sequence where name='User_Websites';
+
+DELETE FROM Users;
+delete from sqlite_sequence where name='Users';
+
+DELETE FROM Websites;
+delete from sqlite_sequence where name='Websites';

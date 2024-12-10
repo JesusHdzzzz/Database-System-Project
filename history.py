38b6c993a8f4154c8751ba1fac93003c3e00aecd
Jesus Hernandez <114837227+JesusHdzzzz@users.noreply.github.com>
2024-12-09 19:04:04 -0800

    Added new message

--- history.py
+++ history.py
@@ -4,7 +4,7 @@ def viewPassHistory(conn):
   try:
     cursor = conn.cursor()
 
-    # Print all websites that the user has saved passwords for
+    # Print all the details of the user's password history
     cursor.execute("""
         SELECT action_type, action_details, action_timestamp
         FROM history, users
@@ -12,11 +12,14 @@ def viewPassHistory(conn):
         AND users.user_id = history.user_id
         AND history.user_id = ?;
         """, (config.user_id,))
-    #print("config.user_id:", config.user_id)
-    #print("config.username:", config.username)
+
+    # Let user know if they have no password history
+    if cursor.rowcount == -1:
+        print("You have no password history")
+
+    # Print info about each password
     for row in cursor.fetchall():
             print(f"- {row[0]} | {row[1]} | {row[2]}")
-
 
   except sqlite3.Error as e:
     print("Database error: ", e)
@@ -33,8 +36,12 @@ def viewCardHistory(conn):
         AND users.user_id = history.user_id
         AND history.user_id = ?;
             """, (config.user_id,))
-        #print("config.user_id:", config.user_id)
-        #print("config.username:", config.username)
+
+        # Let user know if they have no password history
+        if cursor.rowcount == -1:
+            print("You have no card history")
+
+        # Print info about each card    
         for row in cursor.fetchall():
             print(f"- {row[0]} | {row[1]} | {row[2]}")
             
